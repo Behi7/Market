308846fbaf1aed843aa7459ebf5f40f5ef6b275d
behruzbekk <162551627+Behi7@users.noreply.github.com>
2024-12-10 23:16:26 +0500

    Delete Goods directory

--- Goods/authentication/back-office/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('product/', include('Goods.back-office.product.urls')),
-    path('category/', include('Goods.back-office.category.urls')),
-]
--- Goods/authentication/wish/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.wishList, name='wishList'),
-    path('<int:id>', views.addOrDelWish, name='wishP'),
-    path('del<int:id>', views.delWish, name= 'delWish')
-]
